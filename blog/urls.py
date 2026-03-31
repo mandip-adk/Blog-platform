9831417c97cebdcf42af1037dd5328bbb72ed4f9
Mandip <mandipadhikari.codes@gmail.com>
2026-04-01 00:05:42 +0545

    feat: User can create and post the blog from website without admin interference

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import blog_details, blog_list
+from .views import blog_details, blog_list, create_post
 from .apiviews import BlogViewSet
 from rest_framework import routers
 
@@ -10,7 +10,7 @@ router.register(r"post", BlogViewSet)
 urlpatterns = [
     path("blog_details/<int:pk>/", blog_details, name="blog_details"),
     path("blog_list/", blog_list, name= "blog_list" ),
-
+    path("create_post/", create_post, name= "post"),
     #For API
     path("", include(router.urls)),
     
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Blog
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
+from .forms import BlogForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 #Lists all blog post
 def blog_list(request):
@@ -13,3 +16,19 @@ def blog_details(request, pk):
     posts = get_object_or_404(Blog, pk=pk)
     return render(request, "blog/blog_details.html", {"details_of_blogs":posts})
 
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES) # include files for image upload
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.author= request.user
+            blog.save()
+            messages.success(request, "Successfully uploaded the blog")
+            return redirect('blog_list')
+        else:
+            messages.error(request,"There is some error while uploading the blog. Please check every field or try again!")
+    else:
+        form = BlogForm()
+    return render(request, "blog/create_post.html", {"form":form})       
+    
