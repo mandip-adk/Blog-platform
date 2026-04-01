444922e50014268bd85b7401463c8a31e5bcfec5
Mandip <mandipadhikari.codes@gmail.com>
2026-04-01 16:07:36 +0545

    feat: added edit delete post option for author

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import blog_details, blog_list, create_post
+from .views import blog_details, blog_list, create_post, edit_post, delete_post
 from .apiviews import BlogViewSet
 from rest_framework import routers
 
@@ -11,6 +11,8 @@ urlpatterns = [
     path("blog_details/<int:pk>/", blog_details, name="blog_details"),
     path("blog_list/", blog_list, name= "blog_list" ),
     path("create_post/", create_post, name= "post"),
+    path("blog/post/<int:pk>/edit/", edit_post, name="edit_post"),
+    path("blog/post/<int:pk>/delete", delete_post, name= "delete_post"),
     #For API
     path("", include(router.urls)),
     
--- blog/views.py
+++ blog/views.py
@@ -32,3 +32,36 @@ def create_post(request):
         form = BlogForm()
     return render(request, "blog/create_post.html", {"form":form})       
     
+@login_required
+def edit_post(request,pk):
+    post = get_object_or_404(Blog, pk=pk)
+    if request.user != post.author:
+        messages.error(request, "You are not authorized to edit this post.")
+        return redirect('blog_list')
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully uploaded the blog")
+            return redirect('blog_list')
+        else:
+           messages.error(request, "There was an error updating the blog. Please check the fields.")
+    else:
+        form = BlogForm(instance=post)
+    return render(request, 'blog/edit_post.html', {'form': form , 'blog': post})
+
+@login_required
+def delete_post(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+    if request.user != post.author:
+        messages.error(request, "You are not authorized to delete this post.")
+        return redirect('blog_list')
+    
+    if request.method == 'POST':
+        post.delete()
+        messages.success(request, "Post deleted successfully.")
+        return redirect('blog_list')
+
+    return render(request, 'blog/delete_post.html', {'post': post})
+
