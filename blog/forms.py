a99b683852fe89b215587e0f95f02ae25cc8d3fb
Mandip <mandipadhikari.codes@gmail.com>
2026-05-15 08:14:05 +0545

    fix: create post problem.

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ClearableFileInput
 from .models import Blog, Comment
 
 class BlogForm(ModelForm):
     class Meta:
         model = Blog
         fields = ["title", "content", "image"]
+        widgets = {
+            'image': ClearableFileInput(),
+        }
 
 class CommentForm(ModelForm):
     class Meta:
